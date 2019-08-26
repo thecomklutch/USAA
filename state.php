204afb856e84344eb8d8c9c6d664e560e04aef62
L-Andy <lutaayaandrew200@gmail.com>
2019-08-26 03:09:50 +0100

    Deleting state file

--- state.php
+++
@@ -1,7 +0,0 @@
-<?php if(count($states) > 0) : ?>
-	<div class="message error validation_error">
-		<?php foreach ($states as $state): ?>
-			<p><div class="w3-panel w3-small w3-sand w3-round" style="margin-left: 30px; margin-right: 30px;"><h2 class="w3-small w3-text-blue" style="margin: 10px;"><?php echo $state; ?></h2></div></p>
-		<?php endforeach; ?>
-	</div>
-<?php endif; ?>
