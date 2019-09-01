4e1d627bb27beff6f45ee93c9908ef1f8319a30c
unknown <leenswitts@gmail.com>
2019-09-01 01:08:11 -0700

    index page

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
-<div class="w3-container" style="margin: 0 auto;">
-    this is going to be the landing page 
-</div>
+<html>
+<head>
+<title>Home</title>
+<link rel="icon" href="USAAstudent/images/usaa.ico">
+<link rel="stylesheet" type="text/css" href="style_sheets/index.css">
+<script
+			  src="https://code.jquery.com/jquery-3.4.1.js"
+			  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+			  crossorigin="anonymous">
+</script>
+ <link href="style_sheets/fontawesome-fre-5.10.2-web/css/fontawesome.css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+<body>
+    <div class ="nav">
+        <header>
+            <nav>
+            <div class ="menu-icon">
+                <i class="fa fa-bars fa-2x">
+            </div>
+            <div class ="logo">
+                <img src ="usaastudent/images/usaabadge.png"  height="100px">
+            </div>
+            <div class ="menu">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">News</a></li>
+                    <li><a href="">Gallery</a></li>
+                    
+                </ul>
+            </div>
+            
+
+            </nav>
+        </header>
+    </div>
+</body>
+</html>
